1a1d6b1f557000f127ae683150a824a24a2cd500
Fran Arjona Bals <francesc_arjona@ceroca.cat>
2025-02-17 15:45:00 +0100

    Ultimos cambios registro

--- app/views/auth/register.php
+++ app/views/auth/register.php
@@ -21,22 +21,30 @@
             <form action="index.php?action=register" method="POST">
                 <label for="username">Usuario:</label>
                 <input type="text" id="username" name="username" required>
+                
                 <label for="email">Correo Electrónico:</label>
                 <input type="email" id="email" name="email" required>
+                
                 <label for="password">Contraseña:</label>
                 <input type="password" id="password" name="password" required>
+                
                 <label for="age">Edad:</label>
                 <input type="number" id="age" name="age" required>
+                
                 <label for="weight">Peso (kg):</label>
                 <input type="number" step="0.1" id="weight" name="weight" required>
+                
                 <label for="height">Altura (cm):</label>
                 <input type="number" step="0.1" id="height" name="height" required>
+                
                 <label for="goal">Objetivo:</label>
                 <select id="goal" name="goal" required>
+                    <option value="" selected disabled>Selecciona un objetivo</option>
                     <option value="perder_peso">Perder Peso</option>
                     <option value="ganar_masa_muscular">Ganar Masa Muscular</option>
                     <option value="mejorar_resistencia">Mejorar Resistencia</option>
                 </select>
+                
                 <button type="submit">Registrarse</button>
             </form>
         </section>
