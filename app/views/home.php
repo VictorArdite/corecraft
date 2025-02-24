e7e963414889154c8d6501da4d8b1bc6c79cb770
Fran Arjona Bals <francesc_arjona@ceroca.cat>
2025-02-24 18:34:06 +0100

    Cambios botones pantalla inicial

--- app/views/home.php
+++ app/views/home.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CoreCraft - Inicio</title>
     <link rel="stylesheet" href="css/global.css">
-    <link rel="stylesheet" href="css/home.css"> <!-- Ruta  al archivo CSS -->
+    <link rel="stylesheet" href="css/home.css"> <!-- Ruta al archivo CSS -->
 </head>
 <body>
     <header>
@@ -17,12 +17,14 @@
         <nav>
             <ul>
                 <li><a href="index.php?action=home">Inicio</a></li>
-                <li><a href="index.php?action=login">Iniciar Sesión</a></li>
-                <li><a href="index.php?action=register">Registrarse</a></li>
                 <li><a href="index.php?action=rutinas">Rutinas</a></li>
                 <li><a href="index.php?action=suplementacion">Suplementación</a></li>
                 <li><a href="index.php?action=perfil">Perfil</a></li>
             </ul>
+            <div class="auth-buttons">
+                <a href="index.php?action=login" class="auth-button">Iniciar Sesión</a>
+                <a href="index.php?action=register" class="auth-button">Registrarse</a>
+            </div>
         </nav>
     </header>
     <main>
