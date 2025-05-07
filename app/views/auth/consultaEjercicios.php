b2911e8f1e3168d5fdfb443b72c07addd71aa3bd
Fran Arjona Bals <francesc_arjona@ceroca.cat>
2025-05-07 17:54:41 +0200

    Cambios

--- app/views/auth/consultaEjercicios.php
+++ app/views/auth/consultaEjercicios.php
@@ -23,6 +23,8 @@
                 <?php if (isset($_SESSION['user_id'])): ?>
                     <li><a href="index.php?action=registro-peso">Registro de Pesos</a></li>
                 <?php endif; ?>
+                <li><a href="index.php?action=mis-rutinas">Mis Rutinas</a></li>
+                <li><a href="index.php?action=calculadora-nivel">Calculadora de Nivel</a></li>
                 <li><a href="index.php?action=suplementacion">Suplementación</a></li>
                 <li><a href="index.php?action=perfil">Perfil</a></li>
             </ul>
@@ -69,46 +71,6 @@
             </section>
         <?php endforeach; ?>
     </main>
-
-    <footer>
-        <div class="footer-container">
-            <!-- Apartado Compañía -->
-            <div class="footer-section">
-                <h4>Compañía</h4>
-                <div class="footer-links">
-                    <a href="index.php?action=nosotros">Nosotros</a>
-                    <a href="index.php?action=services">Nuestros servicios</a>
-                    <a href="index.php?action=privacy">Política de privacidad</a>
-                </div>
-            </div>
-
-            <!-- Apartado Ayuda -->
-            <div class="footer-section">
-                <h4>Ayuda</h4>
-                <div class="footer-links">
-                    <a href="index.php?action=faq">Preguntas frecuentes</a>
-                    <a href="index.php?action=contact">Contacto</a>
-                </div>
-            </div>
-
-            <!-- Apartado Síguenos -->
-            <div class="footer-section">
-                <h4>Síguenos</h4>
-                <div class="footer-social">
-                    <a href="https://www.facebook.com/profile.php?id=61574264483708" target="_blank">
-                        <i class="fab fa-facebook"></i>
-                    </a>
-                    <a href="https://www.instagram.com/corecraft__/" target="_blank">
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                    <a href="https://x.com/Corecraft__" target="_blank">
-                        <i class="fab fa-twitter"></i>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <p>&copy; 2025 CoreCraft. Todos los derechos reservados.</p>
-    </footer>
 
     <div id="ejercicioModal" class="modal">
         <div class="modal-content">
