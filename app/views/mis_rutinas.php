04e4cf7373d0732658a539d728163d014397dcbb
Fran Arjona Bals <francesc_arjona@ceroca.cat>
2025-05-08 15:20:23 +0200

    Cambios esteticos en todos los apartados

--- app/views/mis_rutinas.php
+++ app/views/mis_rutinas.php
@@ -19,10 +19,25 @@
             <ul>
                 <li><a href="index.php?action=home">Inicio</a></li>
                 <li><a href="index.php?action=rutinas">Rutinas</a></li>
+                <li><a href="index.php?action=consultaEjercicios">Ejercicios</a></li>
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    <li><a href="index.php?action=registro-peso">Registro de Pesos</a></li>
+                <?php endif; ?>
                 <li><a href="index.php?action=mis-rutinas">Mis Rutinas</a></li>
+                <li><a href="index.php?action=calculadora-nivel">Calculadora de Nivel</a></li>
                 <li><a href="index.php?action=suplementacion">Suplementación</a></li>
                 <li><a href="index.php?action=perfil">Perfil</a></li>
             </ul>
+            <?php if (!isset($_SESSION['user_id'])): ?>
+            <div class="auth-buttons">
+                <a href="index.php?action=login" class="auth-button">Iniciar Sesión</a>
+                <a href="index.php?action=register" class="auth-button">Registrarse</a>
+            </div>
+            <?php else: ?>
+            <div class="auth-buttons">
+                <a href="index.php?action=logout" class="auth-button">Cerrar Sesión</a>
+            </div>
+            <?php endif; ?>
         </nav>
     </header>
 
